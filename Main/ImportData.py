4984477a7bc10142feaaeedea61007e961035425
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-18 22:14:00 +0330

    Update ImportData.py

---
+++ Main/ImportData.py
@@ -0,0 +1,14 @@
+import docx 
+import os 
+
+def corpus_reader(the_path,):
+    string_corpus = []
+    the_path = the_path
+    set_class = ''
+    for filename in os.listdir(the_path):
+        file_path = the_path+filename
+        loaded_file = open(file_path,'r',encoding = "utf-8") 
+        line_seperated_data = loaded_file.readlines() 
+        set_class = str(filename[:2])
+        string_corpus.append([line_seperated_data[0],set_class])
+    return string_corpus
