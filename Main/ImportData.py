9c8208cd5a6191ead16704b56d3f5dd359355d4a
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-19 21:44:38 +0330

    Update ImportData.py

--- Main/ImportData.py
+++ Main/ImportData.py
@@ -2,20 +2,24 @@
 """
 Created on Sat Oct 19 20:34:07 2019
 
-@author: Mahdi
+@author: Mahdi Rahbar
 """
 
 import docx 
 import os 
 
-def corpus_reader(the_path,):
-    string_corpus = []
-    the_path = the_path
-    set_class = ''
-    for filename in os.listdir(the_path):
-        file_path = the_path+filename
-        loaded_file = open(file_path,'r',encoding = "utf-8") 
-        line_seperated_data = loaded_file.readlines() 
-        set_class = str(filename[:2])
-        string_corpus.append([line_seperated_data[0],set_class])
-    return string_corpus
+class DataImport:
+    def __init__(self, path, keep_text):
+        self.path = path 
+        self.keep_text = keep_text
+
+    def corpus_reader(self):
+        string_corpus = []
+        set_class = ''
+        for filename in os.listdir(self.path):
+            file_path = self.path+filename
+            loaded_file = open(file_path,'r',encoding = "utf-8") 
+            line_seperated_data = loaded_file.readlines() 
+            set_class = str(filename[:2])
+            string_corpus.append([line_seperated_data[0],set_class])
+        return string_corpus
