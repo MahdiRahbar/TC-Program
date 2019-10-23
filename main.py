158dc1e42e6148cfda3814bb2dc7f523670ae082
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-23 14:48:10 +0330

    modified our main module

--- main.py
+++ main.py
@@ -5,24 +5,10 @@ Created on Mon Oct 21 13:47:48 2019
 @author: Mahdi Rahbar
 """
 from tc import ImportData, Preprocessor, classifier
+from PyQt5 import QtCore, QtGui, QtWidgets
+from GUI.gui import Ui_MainWindow
+from app import main
+import sys
 
 if __name__ == "__main__":
-    Path = './test/'
-    save_path = Path
-    keep_text = True
-    doc2txt = ImportData.DocToText(Path,save_path, keep_text)
-    doc2txt.Text_Maker_Save()
-    
-    D = ImportData.DataImport(Path)
-    Data = D.Corpus_Reader()
-
-    Stop_Word_List = ''
-    PreProc = Preprocessor.PreProcessing(Data, Stop_Word_List)
-    PreProc.String_Splitter()
-    PreProc.TextCleaner()
-    PreProc.wordToString()
-    Data = PreProc.Data_Vectorizer()
-    # print(Data.shape)
-    clf = classifier.InputClassifier(Data)
-    print(clf.SVCClassifier())
-    print(clf.Show_Label())
+    main()
