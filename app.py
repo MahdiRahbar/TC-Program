aafcfa5ed700d8e35a7fb500d9406a9e5d912907
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-23 14:46:40 +0330

    added the main application to handle gui with functionalities

---
+++ app.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 11:28:50 2019
+
+@author: Mahdi Rahbar
+"""
+
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QFileDialog,
+                             QMessageBox, QGridLayout, QTableWidgetItem,
+                             QDialog)
+from PyQt5 import QtCore, QtGui, QtWidgets
+from tc import ImportData, Preprocessor, classifier
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QThread, pyqtSlot
+from GUI import gui
+import sys
+from os.path import expanduser
+# from Main.Preprocessor import *
+
+class TCApp(gui.Ui_MainWindow, QMainWindow):
+    
+    def __init__(self):
+        super(TCApp ,self).__init__()
+
+        self.setupUi(self)
+
+        self.user_in = None  # Stores user's data and input
+        self.data_info = None
+        self.init_GUI()
+
+    def init_GUI(self):
+        """
+        Initialize the GUI of application
+        """
+
+        # Threads
+        self.__threads = []
+
+        # Buttons
+        self.Path_Open_Button.clicked.connect(self.get_data_path)
+        self.Classification_Start_Button.clicked.connect(self.Classifier_trggr)
+
+
+    def get_data_path(self):
+        """
+        Gets the dataset path from a user.
+        """
+
+        data_filename = QFileDialog.getExistingDirectory(self, "Open a folder",
+                                                            "",
+                                                            QFileDialog.ShowDirsOnly)
+        # data_filename, _ = QFileDialog.getOpenFileName(self, "Import Document",
+        #                                                "", "All Files"" (*);""; Docx files"" (*.docx);"";Text files (*.txt)")
+        # print(data_filename)
+
+        if data_filename:
+
+            self.Address_Bar.setText(str(data_filename))
+            self.Classification_Start_Button.setEnabled(True)
+
+            # Enable widgets in Read group box
+            for w in self.Classification_Start_Button.children():
+
+                if not isinstance(w, QGridLayout):
+
+                    w.setEnabled(True)               
+
+    def Classifier_trggr(self):
+        """
+        Loads a dataset.
+        """
+
+        self.data_reader = ImportData.Data_Reader(self.Address_Bar.text(), self.Address_Bar.text() , True) # , True if self.header_check.isChecked() else False
+                                      
+        self.data_reader.Text_Maker_Save()
+        # self.data_reader.load_data(True if self.shuffle_box.isChecked() else
+        #                            False, True if self.normalize_box.
+        #                            isChecked() else False)
+
+        self.user_in = self.data_reader.Corpus_Reader()
+
+
+
+        # TODO: Handle exception when it fails to load dataset and show
+        # the error in message box
+        self.set_pbar_range(100)
+
+        self.Stop_Word_List = ''
+        self.PreProc = Preprocessor.PreProcessing(self.user_in, self.Stop_Word_List)
+        self.PreProc.String_Splitter()
+        self.PreProc.TextCleaner()
+        self.PreProc.wordToString()
+        self.Preprocessed_Data = self.PreProc.Data_Vectorizer()
+        # print(Data.shape)
+        self.clf = classifier.InputClassifier(self.Preprocessed_Data)
+
+
+        print(self.clf.SVCClassifier())
+        self.Output_Text.setText(str(self.Display_labels()))
+
+        print(self.clf.Show_Label())
+
+    def Display_labels(self):
+        self.result_text = ''
+        no_counter = 0
+        for label in self.clf.Show_Label():
+            no_counter += 1
+            if label== "fa":
+                self.result_text = self.result_text + str(no_counter)+ ": "+ "فنی" + "\n"
+            if label== "pa":
+                self.result_text = self.result_text + str(no_counter)+ ": "+ "علوم پایه" + "\n"
+            if label== "pe":
+                self.result_text = self.result_text + str(no_counter)+ ": "+ "پزشکی" + "\n"
+            if label== "ho":
+                self.result_text = self.result_text + str(no_counter)+ ": "+ "هنر" + "\n"
+            if label== "en":
+                self.result_text = self.result_text + str(no_counter)+ ": "+ "علوم انسانی" + "\n"
+
+        return self.result_text
+
+        
+
+
+
+
+
+        # show_dialog("Data Status", "Loaded the dataset successfully.",
+        #             QMessageBox.Information)
+
+        # self.update_data_info()
+
+        # self.user_in.class_type = type_of_target(self.user_in.y_train)
+
+        # Enable other tabs' functionalities
+        # self.enable_classify()
+        # self.enable_visualize()
+        # self.enable_model()
+
+    def update_data_info(self):
+        """
+        Updates the data information like no. features, no. samples and etc.
+
+        Parameters
+        ----------
+        hdr_name : list
+            Header names of dataset.
+        """
+        pass
+        # self.data_info = self.data_reader.get_data_info()
+
+        # self.no_samples.setText(str(self.data_info.no_samples))
+        # self.no_features.setText(str(self.data_info.no_features))
+        # self.no_classes.setText(str(self.data_info.no_class))
+
+        # # print(self.data_info.header_names)
+
+        # if len(self.data_info.header_names) != 0:
+
+        #     self.feature_table.setRowCount(self.data_info.no_features)
+
+        #     for i, hdr_name in enumerate(self.data_info.header_names):
+
+        #         self.feature_table.setItem(i, 0, QTableWidgetItem(hdr_name))
+
+
+
+
+    @pyqtSlot(int)
+    def set_pbar_range(self, pbar_rng):
+        """
+        Sets range of the progress bar by the grid search thread.
+
+        Parameters
+        ----------
+        pbar_rng : int
+            range of the progress bar.
+        """
+
+        self.Classification_Progress_Bar.setRange(0, pbar_rng)
+
+    @pyqtSlot(int, str, str, str)
+    def update_gs_info(self, pbar_val): #  ,curr_acc, best_acc, elapsed_t
+        """
+        Updates current value of the progress bar, current accuracy, and best
+        accuract, elapsed time.
+
+        Parameters
+        ----------
+        pbar_val : int
+            value of progress bar.
+
+        curr_acc
+        """
+
+        self.Classification_Progress_Bar.setValue(pbar_val)
+        # self.acc.setText(curr_acc)
+        # self.best_acc.setText(best_acc)
+        # self.elapsed_time.setText(elapsed_t)
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    tcapp = TCApp()
+    tcapp.show()
+    sys.exit(app.exec_())
+
+main()
+
+
+    # app = QtWidgets.QApplication(sys.argv)
+    # MainWindow = QtWidgets.QMainWindow()
+    # ui = Ui_MainWindow()
+    # ui.setupUi(MainWindow)
+    # MainWindow.show()
+    # sys.exit(app.exec_())
