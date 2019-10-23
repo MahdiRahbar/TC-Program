4fc84b68676db4267d4690de3848d82fb7865815
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-24 02:23:11 +0330

    updated the classifier to show the probablity

--- tc/classifier.py
+++ tc/classifier.py
@@ -10,11 +10,12 @@ from sklearn import svm
 
 class InputClassifier:
 
-    model = joblib.load('./tc/model/SVCmodel.sav')
+    model = joblib.load('./tc/model/SVCmodel_Proba.sav')
     labels = joblib.load('./tc/model/label_encoder.joblib')
     def __init__(self, imported_data):
         self.imported_data = imported_data
         self.Fin_Results = []
+        self.clf_labels = ['انسانی','فنی','هنر','پایه','پزشکی']
     
     def Show_Label(self):
         le = InputClassifier.labels
@@ -23,5 +24,17 @@ class InputClassifier:
     def SVCClassifier(self):
         self.Fin_Results = InputClassifier.model.predict(self.imported_data)
         return self.Fin_Results 
+
     def SVC_Proba(self):
         return InputClassifier.model.predict_proba(self.imported_data)
+
+    def Display_Proba(self):
+        Proba_text = ''
+        Proba_M = self.SVC_Proba()
+        I , J = Proba_M.shape
+        for i in range(I):
+            Proba_text +='%d :  |  '%i 
+            for j in range(J):
+                Proba_text = Proba_text + self.clf_labels[j] + ": %.2f " % Proba_M[i,j] + '  |  '
+            Proba_text += '\n'
+        return Proba_text
