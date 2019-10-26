7c20e2d72f433474675bc0ea2349a8536125872d
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-26 23:21:21 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@
 Created on Sat Oct 26 13:31:12 2019
 
 @author: Mahdi Rahbar
+
+This installation file was made by help of the following link: 
+https://stackoverflow.com/questions/41570359/how-can-i-convert-a-py-to-exe-for-python
 """
 
 
 
 from cx_Freeze import setup, Executable
 
-base = None    
+
+base = "Win32GUI"    
 
 executables = [Executable("main.py", base=base)]
 
-packages = ["idna","tc","GUI"]
+packages = ["idna","PyQt5","numpy","joblib","sklearn","pandas","hazm","pickle",
+            "re","string","collections","ntpath","docx","os"]# ,"PyQt5.QtGui","PyQt5.QtCore","PyQt5.QtWidgets"
 options = {
     'build_exe': {    
         'packages':packages,
