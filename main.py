d8c1bd625d5eed7eba50df5365e519c2aa4c83e6
Mahdi Rahbar <30546576+MahdiRahbar@users.noreply.github.com>
2019-10-28 22:53:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ Created on Mon Oct 21 13:47:48 2019
 
 @author: Mahdi Rahbar
 """
-from tc import ImportData, Preprocessor, classifier
 from PyQt5 import QtCore, QtGui, QtWidgets
-from GUI.gui import Ui_MainWindow
 from app import main
 import sys
 
